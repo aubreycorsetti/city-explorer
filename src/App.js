9481464f25b32fe4177b8404577b5ff7e96e8770
Aubrey Corsetti <aubreylcorsetti@icloud.com>
2022-11-14 20:28:10 -0800

    fixed syntax error

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Alert from 'react-bootstrap/Alert'
+//eslint-disable-next-line no-unused-vars
 import Apps from './Apps.css'
 
 
