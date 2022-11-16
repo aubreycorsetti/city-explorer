3ac4ccf0f4a69a7c6510ca609a71139be280da7b
Aubrey Corsetti <aubreylcorsetti@icloud.com>
2022-11-16 14:47:17 -0800

    added weather data

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return (
+      <>
+        {this.props.date}
+        {this.props.description}
+      </>
+    )
+
+  }
+}
+
+export default Weather;
